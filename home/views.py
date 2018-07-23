954ffb8c6836bac392d3e9aace28ad0781cc0e8b
Osama Ismail Awad <osama.ismail@codeforiraq.org>
2018-07-23 20:29:19 +0300

    1- index.html orginized 2- views.py sorted by date

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from . models import Announcement
 
 
 def index(request):
-    announcements = Announcement.objects.all()
+    announcements = Announcement.objects.all().order_by('create')
     context = {'announcements': announcements}
     return render (request, 'home/index.html', context)
